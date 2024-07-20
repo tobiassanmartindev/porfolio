8504bf646480e0fed9b8885b979fbdc026dd643b
ziggsmachine <ziggsmachine@gmail.com>
2024-07-20 17:42:15 -0300

    navbar responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,11 @@
   <nav class="border-gray-200 fixed w-full z-20 top-2 start-0 z-50 bg-transparent">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
       <div
-        class="w-full md:block md:w-auto py-2 px-5 backdrop-blur-sm bg-white/60 rounded-full shadow"
+        class="w-full block w-auto py-2 px-5 backdrop-blur-sm bg-white/60 rounded-full shadow"
         id="navbar-default"
       >
         <ul
-          class="font-medium flex flex-col md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700"
+          class="font-medium flex flex-row space-x-8 rtl:space-x-reverse rounded-lg md:mt-0 md:border-0 dark:border-gray-700"
         >
           <li>
             <a
