d93d010627eb794a3c0c0673550599ef9f662e9d
Your Name <ziggsmachine@gmail.com>
2024-09-26 18:41:08 -0300

    -

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -2,7 +2,7 @@
   <footer class="bg-white rounded-lg shadow m-4">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-500 sm:text-center">
-        © 2024 <a href="https://flowbite.com/" class="hover:underline">Tobías Sanmartín</a>. Casi
+        © 2024 <a href="https://www.linkedin.com/in/tob%C3%ADas-sanmart%C3%ADn-001b74254/" class="hover:underline">Tobías Sanmartín</a>. Casi
         todos los derechos reservados.
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
